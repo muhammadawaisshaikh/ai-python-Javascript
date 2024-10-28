92555922d89bd6a61b4a4c324a7c6438543828cf
Muhammad Awais <awaisshaikh94@gmail.com>
2024-10-28 19:01:06 +0500

    Feature/gemini text prompt (#4)

--- Angular/src/components/text-based-gemini/text-based-gemini.component.ts
+++ Angular/src/components/text-based-gemini/text-based-gemini.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GeminiGoogleAiService } from '../../services/gemini-google-ai/gemini-google-ai.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-text-based-gemini',
@@ -8,12 +9,41 @@ import { GeminiGoogleAiService } from '../../services/gemini-google-ai/gemini-go
   templateUrl: './text-based-gemini.component.html',
   styleUrl: './text-based-gemini.component.scss'
 })
-export class TextBasedGeminiComponent {
+export class TextBasedGeminiComponent implements OnInit, OnDestroy {
+  private subscription: Subscription | null = null;
+
   constructor(
     private genAiService: GeminiGoogleAiService
   ) { }
 
-  askGemini() {
-    // consume service method to interact with gemini
+  ngOnInit(): void {
+    this.askGemini('Hey, How are you doing today?');
+  }
+
+  /**
+   * Communication with Gemini using Frontend Compatible Service
+   * @param text 
+   */
+  askGemini(text: string) {
+    this.subscription = this.genAiService.askGemini(text).subscribe({
+      next: (response: string) => {
+        console.log(response);
+        alert(`Response from Gemini: ${response}`);
+      },
+      error: (error: any) => {
+        console.error('Error:', error);
+        alert('An error occurred while fetching the response from Gemini.');
+      },
+      complete: () => {
+        console.log('Gemini response completed successfully.');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from the Observable if it exists
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
--- Angular/src/services/gemini-google-ai/gemini-google-ai.service.ts
+++ Angular/src/services/gemini-google-ai/gemini-google-ai.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { GoogleGenerativeAI } from '@google/generative-ai';
+import { environment } from '../../environments/environment';
+import { asyncScheduler, from, map, Observable, of, scheduled } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeminiGoogleAiService {
 
-  constructor(
-    private genAi: GoogleGenerativeAI
-  ) { }
+  // instance to initiate Gemini - Google Ai with API_KEY
+  private genAI: GoogleGenerativeAI;
 
-  geminiText(){
-    // get data from gemini using text prompt
+  constructor() {
+    this.genAI = new GoogleGenerativeAI(environment.googleAiKey);
+  }
+
+  /**
+   * Communicate with Gemini - Google Ai using text prompt
+   */
+  askGemini(prompt: string): Observable<string> {
+    const model: any = this.genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    // converting Promise to Observable - latest way "After Deprecation"
+    return scheduled(from(model.generateContent(prompt)), asyncScheduler).pipe(
+      map((result: any) => result.response.text())
+    );
   }
 }
