9c822c6b700c58ba24075453bcaf9a88818b43a3
Muhammad Awais <awaisshaikh94@gmail.com>
2024-10-28 17:04:40 +0500

    app init client-side only

--- Angular/README.md
+++ Angular/README.md
@@ -1,4 +1,4 @@
-# AngularAiRecipes
+# AngularGeminiRecipes
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.4.
 
--- Angular/src/app/app.component.spec.ts
+++ Angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-ai-recipes' title`, () => {
+  it(`should have the 'angular-gemini-recipes' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ai-recipes');
+    expect(app.title).toEqual('angular-gemini-recipes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-ai-recipes');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-gemini-recipes');
   });
 });
