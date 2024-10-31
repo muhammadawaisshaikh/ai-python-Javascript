a9868589a23a98b2db32faca0e9f3f0a6316e3f0
Sonu Kapoor <sonukapoor@users.noreply.github.com>
2024-10-31 09:24:23 +0100

    Move folder and add error handler (#7)

--- Angular/src/app/services/gemini-google-ai/gemini-google-ai.service.ts
+++ Angular/src/app/services/gemini-google-ai/gemini-google-ai.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
