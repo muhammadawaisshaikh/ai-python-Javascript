b4ed2ad5c07fe22690fa540ec304df4c33950165
Muhammad Awais <awaisshaikh94@gmail.com>
2024-10-31 20:03:48 +0500

    home with authors and details

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -3,8 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'starter',
+    redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then(
+        (c) => c.HomeComponent,
+      ),
   },
   {
     path: 'starter',
